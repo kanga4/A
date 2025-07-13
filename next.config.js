2f18ff5d56b75782e8a8669baab5ae118e86c8a2
kanga4 <tkangamargaret@gmail.com>
2025-07-14 01:20:30 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your other config options
+  // Make sure there's no port setting here that would override your server.js
+};
+
+module.exports = nextConfig;
